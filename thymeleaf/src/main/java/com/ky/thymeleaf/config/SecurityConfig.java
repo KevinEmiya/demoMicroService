b3237033d3b09b951aa361ad3d0ccc65b24728ef
kevin_yang1988 <kevin_yang1988@163.com>
2018-10-05 13:40:41 +0800

    添加security支持

---
+++ thymeleaf/src/main/java/com/ky/thymeleaf/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.ky.thymeleaf.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/css/**", "/js/**", "/fonts/**", "/index").permitAll()
+        .antMatchers("/users/**").hasRole("ADMIN")
+        .and().formLogin().loginPage("/login").failureUrl("/login-error");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // 在內存中添加一个测试用管理员用户
+        auth.inMemoryAuthentication()
+                .withUser("kevin").password("123456").roles("ADMIN");
+    }
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+}
